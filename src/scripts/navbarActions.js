c10a7f8928b485284c252ea1e77ba0a1e627c3b9
DarinelGuillen <darinel.escobar@outlook.com>
2024-03-09 13:31:59 -0600

    [Structure] cambio de estrcutura no funciona todavia

---
+++ src/scripts/navbarActions.js
@@ -0,0 +1,10 @@
+// navbarActions.js
+document.addEventListener('DOMContentLoaded', (event) => {
+  document.getElementById('btn1').addEventListener('click', function () {
+    alert('Botón 1 presionado');
+  });
+
+  document.getElementById('btn2').addEventListener('click', function () {
+    alert('Botón 2 presionado');
+  });
+});
---
+++ src/scripts/popup.js
@@ -0,0 +1,7 @@
+// popup.js
+document.addEventListener('DOMContentLoaded', function() {
+  var actionButton = document.getElementById('actionButton');
+  actionButton.addEventListener('click', function() {
+      alert('Botón clickeado');
+  });
+});
